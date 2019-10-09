6def7879349352be96d1e4d7c3350f406a7bee41
Abdulla Mian <miana@claycorp.com>
2019-10-09 16:57:20 -0500

    walking skeleton

---
+++ ToDoAppConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To To-Do App");
+        }
+    }
+}
---
+++ ToDoAppTests/CreateToDoTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using ToDoApp;
+
+namespace ToDoAppTests
+{
+    public class CreateToDoTests
+    {
+        [Test]
+        public void CreateToDo()
+        {
+            CreateToDo create_todo = new CreateToDo();
+        }
+
+    }
+}
