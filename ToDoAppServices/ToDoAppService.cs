e362b4b50aa71c0f3447aadf0d369dad1e1cfef8
Abdulla Mian <miana@claycorp.com>
2019-10-27 17:46:26 -0500

    created tests to save a list and task to a DB

--- ToDoAppServices/ToDoAppService.cs
+++ ToDoAppServices/ToDoAppService.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using static ToDoAppDomain.Classes.DomainClasses;
 
 namespace ToDoAppServices
 {
     public class ToDoAppService
     {
+        private ToDoContext _context;
+
+        public ToDoAppService(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        public List CreateList(string listName)
+        {
+            var list = _context.Lists.Add(new List { ListName = listName });
+            _context.SaveChanges();
+
+            return list;
+        }
+
+        public Task CreateTask(string taskDescription, bool taskStatus, DateTime completionDate)
+        {
+            var task = _context.Tasks.Add(new Task { TaskDescription = taskDescription, TaskStatus = taskStatus, CompletionDate = completionDate });
+            _context.SaveChanges();
+
+            return task;
+        }
+
     }
 }
---
+++ ToDoAppTests/ToDoAppServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using ToDoAppServices;
+using static ToDoAppDomain.Classes.DomainClasses;
+using List = ToDoAppDomain.Classes.DomainClasses.List;
+
+namespace ToDoAppTests
+{
+    class ToDoAppServiceTests
+    {
+        private ToDoAppService tds;
+
+        [Test]
+        public void SaveListToDatabase()
+        {
+            var mockSet = new Mock<DbSet<List>>();
+
+            var mockContext = new Mock<ToDoContext>();
+            mockContext.Setup(m => m.Lists).Returns(mockSet.Object);
+
+            var service = new ToDoAppService(mockContext.Object);
+            service.CreateList("Road Trip");
+
+            mockSet.Verify(m => m.Add(It.IsAny<List>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+
+        }
+
+        [Test]
+        public void SaveTaskToDatabase()
+        {
+            var mockSet = new Mock<DbSet<Task>>();
+
+            var mockContext = new Mock<ToDoContext>();
+            mockContext.Setup(m => m.Tasks).Returns(mockSet.Object);
+
+            var service = new ToDoAppService(mockContext.Object);
+            service.CreateTask("Visit Delaware", false, DateTime.Today);
+
+            mockSet.Verify(m => m.Add(It.IsAny<Task>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+
+        }
+
+    }
+}
